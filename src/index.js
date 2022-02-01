eb95798222e3b6cceafe5292bf62dcc3f7e36a96
aaronfraint <aaronfraint@icloud.com>
2022-02-01 15:54:09 -0500

    wire up new functionality

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import "./css/style.css";
 import { map } from "./js/map";
 import { data_sources } from "./js/sources";
 import { layers } from "./js/layers";
-import "./js/form";
+import { use_pointer, click_on_trailheads } from "./js/user_interaction";
+import { assign_form_logic } from "./js/form";
 
 map.on("load", () => {
   for (const src in data_sources) map.addSource(src, data_sources[src]);
 
   for (const lyr in layers) map.addLayer(layers[lyr]);
+
+  use_pointer(map);
+  click_on_trailheads(map);
+
+  assign_form_logic(map);
 });
